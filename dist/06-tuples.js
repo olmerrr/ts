f0983507097802b41a348aff85ee5c08bd933bf0
Yaroslav <dombrovskiy91@gmail.com>
2023-05-25 16:35:57 +0200

    tuples base

---
+++ dist/06-tuples.js
@@ -0,0 +1,18 @@
+"use strict";
+// tuples
+const pairs = [
+    ["key1", "value1"],
+    ["key2", "value2"],
+    ["key3", "value3"],
+];
+const pairsSec = [
+    ["key1", 1],
+    ["key2", 2],
+    ["key3", 3],
+];
+const pairsThird = [
+    [1, true, "value1"],
+    [2, false, "value2"],
+    [3, true, "value3"],
+];
+pairsThird.push("some?"); //
