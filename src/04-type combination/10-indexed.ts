8a8665fc13c9451c62b4200ce77f3e7908583f02
Yaroslav <dombrovskiy91@gmail.com>
2023-05-30 15:55:43 +0200

    indexed
    indexed

--- src/04-type combination/10-indexed.ts
+++ src/04-type combination/10-indexed.ts
@@ -1,1 +1,9 @@
 // indexed
+export {}
+// тип можно получать по индексу
+type SomeTuple = [number, string, boolean]
+type T1 = SomeTuple[0] // number
+
+const sizes = ["s","m","l"] as const;
+type T2 = typeof sizes[number];
+// indexed
