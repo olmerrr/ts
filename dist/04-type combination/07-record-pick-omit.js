37379d13e59652250286cb3dafe616efc6450dd0
Yaroslav <dombrovskiy91@gmail.com>
2023-05-30 14:20:19 +0200

    readonly-required-partial
    readonly-required-partial

--- dist/04-type combination/07-record-pick-omit.js
+++ dist/04-type combination/07-record-pick-omit.js
@@ -1,2 +1,12 @@
 "use strict";
+const myBrandsNew = {
+    zara: ""
+};
+const visitedCountries = {
+    Africa: ['Egypt', 'Angola', 'Tunis'],
+    America: ['Mexico'],
+    Asia: ['India'],
+    Europe: ['Spain', 'England', 'Hungary'],
+    Oceania: []
+};
 // record pick omit
--- src/04-type combination/07-record-pick-omit.ts
+++ src/04-type combination/07-record-pick-omit.ts
@@ -1,1 +1,51 @@
 // record pick omit
+// Record - утилита для работы с однотипными данными
+// type Names = Record<string, any>
+type Names = Record<string, number>
+type Brands = "prada" | "zara" | "HM";
+
+type BrandNames  = Record<Brands, string>
+const myBrandsNew: BrandNames = {
+  zara: ""
+}
+
+interface NewTodo {
+  id: string,
+  title: string,
+  msg: string,
+  completed: boolean,
+}
+// возьми из NewTodo конкретное значение
+type SimpleTodo = Pick<NewTodo, "title" | "msg">
+
+// возьми из NewTodo все значения кроме тех что я указалконкретное значение
+type SimpleTodoWithOmit = Omit<NewTodo, "title" | "msg">
+
+type Regions = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
+type CountriesByRegion = Record<Regions, string[]>
+
+const visitedCountries: CountriesByRegion = {
+  Africa: ['Egypt', 'Angola', 'Tunis'],
+  America: ['Mexico'],
+  Asia: ['India'],
+  Europe: ['Spain', 'England', 'Hungary'],
+  Oceania: []
+}
+
+// Дан тип Country, который будет использоваться в разных целях.
+//   Мы хотим создать компонент с карточкой, где должны выводиться только названия страны, столица и регион.
+//   Как нам сформировать тип CountryCardProps?
+
+  type Country = {
+    name: string,
+    region: string,
+    population: number,
+    flag: string,
+    capital: string,
+    currencies: string[],
+    languages: string[],
+  }
+
+type CountryCardProps = Pick<Country, "name" | "capital">
+// record pick omit
